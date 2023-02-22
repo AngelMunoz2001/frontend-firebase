59867e16117ce33148dcc51aaa61b06d8d93f79a
AngelFmc <612110393@utsalamanca.edu.mx>
2023-02-22 10:15:37 -0600

    Tarjeta

---
+++ components/user/login.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card class="Tarjeta"> <!--Esta es la tarjeta-->
+        <img src="" alt="">
+        <v-card-title class="Titulo">Inicia Sesion</v-card-title> <!--Este es el titulo-->
+        <v-card-text>
+            <v-form v-model="formularioValido"> <!--Este es el formulario-->
+                <v-text-field label="Correo Electronico" placeholder="Correo electronico" v-model="correoElectronico"/>
+            </v-form>
+        </v-card-text>
+
+        <v-card-actions> <!--Este es para los botones-->
+            Botones
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                formularioValido: false,
+                correoElectronico: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .Tarjeta{
+        width: 600px;
+        height: 300px;
+        background-color: #5c7774;
+    }
+
+    .Titulo{
+        justify-content: center;
+    }
+</style>
---
+++ layouts/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-row class="Orientacion">
+        <nuxt />
+    </v-row>
+</template>
+
+<style scoped>
+    .Orientacion{
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-content: center;
+        align-items: center;
+    }
+</style>
